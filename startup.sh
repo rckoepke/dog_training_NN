cb687457c0340b4809c491796f37cbec52cdc081
Ross Koepke <koepke@gmail.com>
2019-12-31 21:07:34 -0600

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -2,5 +2,5 @@ eval "$(/root/anaconda3/bin/conda shell.bash hook)"
 source activate dlc-ubuntu-GPU
 git clone https://github.com/rckoepke/dog_training_NN.git
 export DLClight=True
-touch "~/startupworked"
-ipython startup.py
+touch /root/startupworked
+ipython /root/dog_training_NN/startup.py
